6a5703ce6adf1691c44b2644834357b0a492b1cd
Bogdan Seliman <selimanbogdan@gmail.com>
2022-03-21 21:03:27 +0200

    Setting Form, input and button in FeedbackForm

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 import Card from './shared/Card'
 
 export default function FeedbackForm() {
+    const [text,setText] = useState('');
+    const handleTextChange = (e)=>{
+        setText(e.target.value)
+    }
   return (
       <div>
             <Card>
                 <form>
                     <h2>How would you rate your service with us??</h2>
                     {/* {todo-rating select component} */}
+                    <div className='input-group'>
+                        <input onChange={handleTextChange} 
+                        type='text' 
+                        placeholder='Write your opinion..'
+                        value={text}
+                        />
+                        <button type='submit'> Send</button>
+                    </div>
                 </form>
             </Card>
       </div>
